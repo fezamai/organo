107cd311f1dbfd2e1cd2947843899f473145a8e0
Fernando da Silva Filho <fsfilho@rd.com.br>
2024-02-19 18:05:35 -0300

    Projeto Organo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    //JSX (Como o React trabalha com a parte visual)
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
+
